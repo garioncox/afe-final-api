d637be2e07d251f5e2f000ff459255e1b808cd03
Garion Cox <garion.cox@students.snow.edu>
2024-11-22 08:51:30 -0700

    Update Budget DTO

--- data/DTO/BudgetDTO.cs
+++ data/DTO/BudgetDTO.cs
@@ -2,6 +2,6 @@ namespace afe_final_api.data.DTO;
 
 public class BudgetDTO
 {
-    public required string budgetName { get; set; } = null!;
+    public required string budgetName { get; set; }
     public required int customerId { get; set; }
 }
